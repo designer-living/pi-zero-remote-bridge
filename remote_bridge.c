0f1decc8009638318be8a982e645cf696304ed69
foxy82 <foxy82.github@gmail.com>
2026-01-28 11:35:51 +0000

    Flush the logs

--- remote_bridge.c
+++ remote_bridge.c
@@ -42,6 +42,7 @@ static int matches_device(int fd, const char *target) {
 
     if (strcmp(name, target) == 0) {
         printf("Matched device: %s\n", name);
+        fflush(stdout);
         return 1;
     }
 
@@ -114,10 +115,13 @@ int main(int argc, char *argv[]) {
     }
     closedir(d);
 
-    if (evfd >= 0)
+    if (evfd >= 0) {
         printf("Remote connected\n");
+        fflush(stdout);
+    }
 
     printf("Waiting for remote matching \"%s\"...\n", remote_name);
+    fflush(stdout);
 
     struct pollfd fds[2];
     struct input_event ev;
@@ -157,6 +161,7 @@ int main(int argc, char *argv[]) {
                         break;
                     if (errno == ENODEV) {
                         printf("Remote disconnected\n");
+                        fflush(stdout);
                         close(evfd);
                         evfd = -1;
                         break;
@@ -203,8 +208,10 @@ int main(int argc, char *argv[]) {
                              INPUT_DIR "/%s", ie->name);
 
                     evfd = try_open_device(path, remote_name);
-                    if (evfd >= 0)
+                    if (evfd >= 0) {
                         printf("Remote connected\n");
+                        fflush(stdout);
+                    }
                 }
 
                 p += sizeof(*ie) + ie->len;
